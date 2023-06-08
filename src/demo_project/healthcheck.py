acda41f47f7d9b5f0fd1393162c4da3fe0046b49
Nathan Nowack <thrast36@gmail.com>
2023-06-08 15:30:41 -0500

    init

---
+++ src/demo_project/healthcheck.py
@@ -0,0 +1,31 @@
+import platform
+import sys
+
+import prefect
+from prefect import task, flow
+from prefect.server.api.server import SERVER_API_VERSION
+
+
+@task
+def log_platform_info():
+    print(
+        f"Host's network name = {platform.node()}\n"
+        f"Python version = {platform.python_version()}\n"
+        f"Platform information (instance type) = {platform.platform()}\n"
+        f"OS/Arch = {sys.platform}/{platform.machine()}\n"
+        f"Prefect Version = {prefect.__version__} 🚀\n"
+        f"Prefect API Version = {SERVER_API_VERSION}\n"
+    )
+
+
+
+@flow(log_prints=True)
+def healthcheck(message: str = "Hello Marvin!"):
+    
+    print(message)
+    
+    log_platform_info()
+
+
+if __name__ == "__main__":
+    healthcheck()
