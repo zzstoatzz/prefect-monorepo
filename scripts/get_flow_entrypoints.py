77577de784ee6126fc5bb9ed89c251dba8ee3e46
Nathan Nowack <thrast36@gmail.com>
2023-07-12 00:25:02 -0500

    dumb

--- scripts/get_flow_entrypoints.py
+++ scripts/get_flow_entrypoints.py
@@ -2,10 +2,11 @@ import pathlib
 from prefect.deployments.base import _search_for_flow_functions
 
 async def main():
-    base_dir = pathlib.Path(__file__).parent.parent
+    base_dir = pathlib.Path(__file__).parent.parent / "src"
     entrypoints = [
-        f"{pathlib.Path(flow['filepath']).relative_to(base_dir)}:{flow['function_name']}"
+        f"src/{pathlib.Path(flow['filepath']).relative_to(base_dir)}:{flow['function_name']}"
         for flow in await _search_for_flow_functions(directory=str(base_dir))
+        if flow['filepath'].startswith(str(base_dir)) # Added condition
     ]
     print(','.join(entrypoints))
 
