0ab1c1de10b302edeffa14b9879cb25d065bfc3e
Nathan Nowack <thrast36@gmail.com>
2023-09-07 01:09:44 -0500

    testing theme

--- src/demo_project/healthcheck.py
+++ src/demo_project/healthcheck.py
@@ -10,19 +10,19 @@ from prefect.states import Completed
 @task
 def log_platform_info():
     print(
-        f"Host's network name = {platform.node()}\n"
-        f"Python version = {platform.python_version()}\n"
-        f"Platform information (instance type) = {platform.platform()}\n"
-        f"OS/Arch = {sys.platform}/{platform.machine()}\n"
-        f"Prefect Version = {prefect.__version__} 🚀\n"
-        f"Prefect API Version = {SERVER_API_VERSION}\n"
+            f"Host's network name = {platform.node()}\n"
+            f"Python version = {platform.python_version()}\n"
+            f"Platform information (instance type) = {platform.platform()}\n"
+            f"OS/Arch = {sys.platform}/{platform.machine()}\n"
+            f"Prefect Version = {prefect.__version__} 🚀\n"
+            f"Prefect API Version = {SERVER_API_VERSION}\n"
     )
 
 @flow(log_prints=True)
 def healthcheck(message: str = "hello, world!"):
-    
+
     print(message)
-    
+
     log_platform_info()
 
     return Completed(message="Healthcheck completed.")
