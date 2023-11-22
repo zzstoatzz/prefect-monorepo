5ff1a454d98552639e664138349fc48366868da0
Nathan Nowack <thrast36@gmail.com>
2023-11-22 04:06:53 -0600

    better handling

--- src/demo_project/daily_flow.py
+++ src/demo_project/daily_flow.py
@@ -1,6 +1,22 @@
+"""Demonstrate how to dynamically set the start and end datetimes of a flow
+that runs daily, and illustrates that retries will occur with the same inputs.
+
+```python
+    # Deploying the flow
+    flow.from_source(
+        source="https://github.com/zzstoatzz/prefect-monorepo.git",
+        entrypoint="src/demo_project/daily_flow.py:daily_flow"
+    ).deploy(
+        name="My Daily Flow Deployment",
+        schedule={"cron": "0 14 * * *"},
+        work_pool_name="prefect-managed"
+    )
+```
+"""
 from datetime import datetime, timedelta
 
 from prefect import flow, task
+from prefect.context import get_run_context
 
 
 @task # doesn't need to be a task, but just to be conspicious
@@ -19,8 +35,9 @@ def get_time_range(
 
 @flow(retries=1)
 def downstream_workflow(start: datetime, end: datetime):
-    global seen_times
-    seen_times.add((start, end))
+    # globals not recommended, we're just showing that retries occur with same inputs
+    global observed
+    observed.update({(start, end): get_run_context().flow_run.run_count})
     print(f"Running downstream workflow from {start} to {end}...")
     raise ValueError("oops i failed this time")
 
@@ -28,22 +45,16 @@ def downstream_workflow(start: datetime, end: datetime):
 def daily_flow(start: datetime | None = None, end: datetime | None = None):
     start, end = get_time_range(start, end)
     
-    global seen_times
-    seen_times = set()
+    global observed # again, globals not recommended
+    observed = dict()
         
     downstream_workflow(start, end, return_state=True) # return state to avoid raising
         
-    assert len(seen_times) == 1 and seen_times.pop() == (start, end)
+    used_one_dt_range = len(observed) == 1 and observed.keys() == {(start, end)}
+    ran_subflow_twice = observed.get((start, end)) == 2
+    
+    if used_one_dt_range and ran_subflow_twice:
+        return "Failed successfully!"
 
 if __name__ == "__main__":
-    daily_flow()
-    
-    ## Deploying the flow
-    # flow.from_source(
-    #     source="https://github.com/zzstoatzz/prefect-monorepo.git",
-    #     entrypoint="src/demo_project/daily_flow.py:daily_flow"
-    # ).deploy(
-    #     name="My Daily Flow Deployment",
-    #     schedule={"cron": "0 14 * * *"},
-    #     work_pool_name="prefect-managed"
-    # )
+    print(daily_flow())
