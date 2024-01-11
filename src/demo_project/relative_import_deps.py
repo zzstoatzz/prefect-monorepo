083f3c6476b484101423c13e30652e2ad15fad68
Nathan Nowack <thrast36@gmail.com>
2024-01-11 06:10:40 -0800

    push example

--- src/demo_project/relative_import_deps.py
+++ src/demo_project/relative_import_deps.py
@@ -3,13 +3,13 @@ from prefect import flow
 
 
 @flow(log_prints=True)
-def uses_relative_helper() -> int:
+def uses_helper() -> int:
     return helper()
 
 if __name__ == "__main__":
-    uses_relative_helper.from_source(
+    uses_helper.from_source(
         source="https://github.com/zzstoatzz/prefect-monorepo.git",
-        entrypoint="src/demo_project/relative_import_deps.py:uses_relative_helper",
+        entrypoint="src/demo_project/relative_import_deps.py:uses_helper",
     ).deploy(
         "uses-relative-helper",
         work_pool_name="k8s",
