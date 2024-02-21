bc155b93ab5ace3d4ca72aab966fb55fc884ada7
Nathan Nowack <thrast36@gmail.com>
2024-02-21 08:03:28 -0600

    add demo flow for docs

---
+++ src/demo_project/demo_flow.py
@@ -0,0 +1,33 @@
+import os
+
+from prefect import flow, task
+
+
+@task
+def do_something_important(not_so_secret_value: str) -> None:
+    print(f"Doing something important with {not_so_secret_value}!")
+
+@flow(log_prints=True)
+def some_work():
+    environment = os.environ.get("EXECUTION_ENVIRONMENT", "local")
+    
+    print(f"Coming to you live from {environment}!")
+    
+    not_so_secret_value = os.environ.get("MY_NOT_SO_SECRET_CONFIG")
+    
+    if not_so_secret_value is None:
+        raise ValueError("You forgot to set MY_NOT_SO_SECRET_CONFIG!")
+
+    do_something_important(not_so_secret_value)
+
+if __name__ == "__main__":
+    some_work.deploy(
+        name="demo-deployment",
+        work_pool_name="local",
+        job_variables={
+            "env": {
+                "EXECUTION_ENVIRONMENT": "{{ $EXECUTION_ENVIRONMENT }}",
+                "MY_NOT_SO_SECRET_CONFIG": "{{ $MY_NOT_SO_SECRET_CONFIG }}"
+            }
+        }
+    )
--- src/demo_project/get_served.py
+++ src/demo_project/get_served.py
@@ -2,12 +2,8 @@ from prefect import flow
 
 
 @flow(log_prints=True)
-def foo(
-    x: int,
-    y: int = 42,
-    z: str | None = None,
-) -> int:
-    print(x, y, z)
+def foo(x: str | None = "oof") -> int:
+    print(f"foo got {x=}")
 
 if __name__ == "__main__":
     foo.serve("foo")
