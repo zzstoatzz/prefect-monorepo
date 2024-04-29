b89e96f6a04fe8f131edaaf83bd73c220c5a075c
zzstoatzz <thrast36@gmail.com>
2024-04-29 18:04:44 -0500

    test

---
+++ repros/12857.py
@@ -0,0 +1,27 @@
+import asyncio
+
+import aiohttp
+from prefect import flow, task
+
+
+@flow
+async def my_flow():
+    for url in ["https://www.google.com", "https://www.microsoft.com"]:
+        data = await download_file.submit(url)
+        do_something_with_data.submit(data)
+
+
+@task()
+async def download_file(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            assert resp.status == 200
+            return await resp.read()
+
+
+@task()
+def do_something_with_data(data): ...
+
+
+if __name__ == "__main__":
+    asyncio.run(my_flow())
