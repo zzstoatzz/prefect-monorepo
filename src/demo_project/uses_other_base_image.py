a519c2034cfe123386ab52971c9f52d8c4b86a3d
zzstoatzz <thrast36@gmail.com>
2024-06-04 10:39:05 -0500

    add example

--- src/demo_project/uses_other_base_image.py
+++ src/demo_project/uses_other_base_image.py
@@ -1,30 +1,24 @@
 import os
 
-import marvin
 from prefect import flow
 from prefect.deployments import DeploymentImage
 
 
 @flow(log_prints=True)
-def say_you_are_healthy(in_the_style_of: str):
-    message = marvin.generate(
-        str, instructions=f"Say 'I am healthy' in the style of {in_the_style_of}"
-    )
-
-    print(message)
+def say_you_are_healthy():
+    print("I am healthy!")
+    print(os.environ["FOO"])
 
 
 if __name__ == "__main__":
     say_you_are_healthy.deploy(
-        name="fun-health-check",
+        name="health-check",
         work_pool_name="docker-work",
         image=DeploymentImage(
-            name="zzstoatzz/marvin-fun-health-check:latest",
+            name="zzstoatzz/health-check:latest",
             dockerfile="Dockerfile.other_base",
         ),
-        job_variables={
-            "env": {
-                "OPENAI_API_KEY": f"{os.getenv('OPENAI_API_KEY')}",
-            }
-        },
+        job_variables={"env": {"FOO": "bar"}},
+        build=False,
+        push=False,
     )
