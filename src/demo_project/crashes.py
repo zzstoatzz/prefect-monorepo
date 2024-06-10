5bb09452772b87355a2af899455578b9c680b23c
zzstoatzz <thrast36@gmail.com>
2024-06-10 12:18:45 -0500

    push

--- src/demo_project/crashes.py
+++ src/demo_project/crashes.py
@@ -3,8 +3,14 @@ from prefect import flow
 
 @flow
 def boom_crash():
-    raise SystemExit("Boom!")
+    raise ValueError
 
 
 if __name__ == "__main__":
-    boom_crash()
+    flow.from_source(
+        source="https://github.com/zzstoatzz/prefect-monorepo.git",
+        entrypoint="src/demo_project/crashes.py:boom_crash",
+    ).deploy(
+        name="boom-crash",
+        work_pool_name="local",
+    )
