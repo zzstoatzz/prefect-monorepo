78fedd832bede0edf43ea8036427e920167bebc9
zzstoatzz <thrast36@gmail.com>
2024-07-22 22:59:33 -0500

    try win example

--- scripts/run_worker_for_a_bit.py
+++ scripts/run_worker_for_a_bit.py
@@ -1,12 +1,16 @@
 import subprocess
+import sys
 
 from prefect.utilities.timeout import timeout
 
 
-def main():
-    with timeout(100):
+def main(timeout_seconds: float = 100.0):
+    with timeout(seconds=timeout_seconds):
         subprocess.run(["prefect", "worker", "start", "--pool", "local"], check=True)
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) > 1:
+        main(float(sys.argv[1]))
+    else:
+        main()
