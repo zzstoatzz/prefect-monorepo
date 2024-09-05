df62ab63f8b5c58f2437fcafe2c12a16cab5e20f
zzstoatzz <thrast36@gmail.com>
2024-09-04 20:38:59 -0500

    maor

--- src/demo_project/test.py
+++ src/demo_project/test.py
@@ -6,16 +6,17 @@ from prefect import flow
 def random_flow():
     print(emoji.emojize("Hello world :sun_with_face:"))
 
+
 if __name__ == "__main__":
     random_flow.from_source(
         source="https://github.com/zzstoatzz/prefect-monorepo.git",
         entrypoint="src/demo_project/test.py:random_flow",
     ).deploy(
         "test-k8s",
-        work_pool_name="k8s",
+        work_pool_name="kubernetes-pool",
         job_variables={
             "env": {
                 "EXTRA_PIP_PACKAGES": "emoji",
             }
-        }
+        },
     )
