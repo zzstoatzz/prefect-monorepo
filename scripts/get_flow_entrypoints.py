589973173a6e306bd1cb21a5526fd91fe0cf5fc0
zzstoatzz <thrast36@gmail.com>
2025-01-18 14:56:52 -0600

    fix CI

--- scripts/get_flow_entrypoints.py
+++ scripts/get_flow_entrypoints.py
@@ -6,21 +6,23 @@
 
 import pathlib
 
-from prefect.deployments.base import _search_for_flow_functions
+from prefect.cli._prompts import search_for_flow_functions
 
 
 async def main():
-    src_dir = pathlib.Path('src')
+    src_dir = pathlib.Path("src")
     base_dir = pathlib.Path(__file__).resolve().parent.parent / src_dir
-    
+
     entrypoints = [
-        f"src/{flow['filepath'].replace(str(base_dir) + '/', '')}:{flow['function_name']}" # noqa: E501
-        for flow in await _search_for_flow_functions(directory=str(base_dir))
-        if str(base_dir) in flow['filepath']
+        f"src/{flow['filepath'].replace(str(base_dir) + '/', '')}:{flow['function_name']}"  # noqa: E501
+        for flow in await search_for_flow_functions(directory=str(base_dir))
+        if str(base_dir) in flow["filepath"]
     ]
 
-    print(','.join(entrypoints))
+    print(",".join(entrypoints))
+
 
 if __name__ == "__main__":
     import asyncio
+
     asyncio.run(main())
